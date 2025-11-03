d9438589a8fe4344b825eb271b6442f2d0da2dd9
Zanna-dev <pamn2704@gmail.com>
2025-11-03 21:46:06 +0100

    Implement Data Fetching in Angular 20

--- src/app/async-task-component/async-task-component.ts
+++ src/app/async-task-component/async-task-component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-async-task-component',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './async-task-component.html',
   styleUrl: './async-task-component.scss',
 })
 export class AsyncTaskComponent {
+  // secondScenario
+  private http = inject(HttpClient);
+  data = toSignal(this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts'));
+
+// Third scenario
+  //  The $ suffix convention means "this is an Observable"
+  posts$: Observable<any> = this.http.get('https://jsonplaceholder.typicode.com/posts');
+
+
+  // message = signal('Waiting for data...');
+  message = signal('Waiting for data...');
+
+  fetchData() {
+    this.message.set('Loading...');
+    setTimeout(() => {
+      this.message.set('Data fetched successfully 🎉');
+    }, 1500);
+  }
+
 
 }
